f4adaf30e4a9f9125388cf43861774c65bbe2cf4
unknown <chap6680@gmail.com>
2019-11-30 12:42:40 -0700

    final mongoose files

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
 const HttpError = require('./models/http-error');
@@ -11,7 +10,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/api/places', placesRoutes); // => /api/places...
+app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
@@ -23,12 +22,16 @@ app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
   }
-  res.status(error.code || 500)
-  res.json({message: error.message || 'An unknown error occurred!'});
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
-mongoose.connect('mongodb+srv://mernmax:glasses@cluster0-hbcwz.mongodb.net/places?retryWrites=true&w=majority').then(()=>{
-  app.listen(5000);
-}).catch(err=>{
-console.log(err);
-});
+mongoose
+  .connect('mongodb+srv://mernmax:glasses@cluster0-hbcwz.mongodb.net/places?retryWrites=true&w=majority')
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
